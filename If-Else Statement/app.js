0c940c86e9e51e5ea39635ec2e8cca664e2a163f
AnasAhmed001 <hafizanasahmed8@gmail.com>
2024-02-29 00:09:41 +0500

    java script grade calculator

---
+++ If-Else Statement/app.js
@@ -0,0 +1,24 @@
+var percentage = +prompt("Enter Your Percentage");
+
+
+if(percentage <= 100 && percentage >= 90) {
+    console.log("Your Grade Is A1");
+
+} elseif(percentage <= 90 && percentage >= 80) {
+    console.log("Your Grade Is A")
+
+} elseif(percentage <= 80 && percentage >= 60) {
+    console.log("Your Grade Is B")
+
+} elseif(percentage <= 60 && percentage >= 40) {
+    console.log("Your Grade Is C")
+
+}
+
+else {
+    console.log("You Are Fail!")
+}
+
+
+
+
